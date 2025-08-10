f9f9892319c816fc11f9ec92fcc5ca28f8ae63a7
cunha95082-svg <cunha95082@gmail.com>
2025-08-10 19:39:28 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 
 int main(){
-printf("Desafio das cartas!\n");
+printf ("Desafio das cartas!\n");
 
+printf ("Carta 1\n");
+char estado1='A';
+char codigo1[20]= "A01";
+char cidade1[20]= "São Paulo";
+int população1= 12325000;
+float Área1= 1521.11;
+float PIB1= 699.28;
+int Pontos1=50;
+printf ("estado1: %c\n",estado1);
+printf ("codigo1: %s\n" , codigo1);
+printf ("cidade1: %s\n", cidade1);
+printf ("população1: %d\n", população1);
+printf ("Área1: %f\n", Área1);
+printf ("PIB1: %f\n", PIB1);
+printf ("Pontos turisticos1: %d\n", Pontos1);
+printf ("\n");
 
-
-
-}
-
+printf("Carta 2\n");
+char estado2='B';
+char codigo2[20]="B02";
+char cidade2[20]="Rio de Janeiro";
+int população2= 6748000;
+float Área2= 1200.25;
+float PIB2= 300.50;
+int Pontos2=30;
